2f68673c8c2a7c315429d3358407920fdf8a9419
Eddy Brown <eddybrown@me.com>
2022-05-21 10:15:59 +0100

    Add JSDoc

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,42 @@ export type SecondaryReducer<State, ActionCreator extends ActionCreatorFn> = (
   action: ReturnType<ActionCreator>
 ) => State;
 
-export const withPrefix = (prefix: string) => (actionType: string) =>
+/**
+ * Create namespaced actions by initialising `withPrefix("NAMESPACE")` somewhere with your actions.
+ *
+ * @param prefix
+ *
+ * @example
+ * const withPrefix = createPrefix("COUNTER_");
+ *
+ * // action.type === "COUNTER_INCREMENT
+ * const action = {
+ *     type: withPrefix("INCREMENT")
+ * }
+ */
+export const createPrefix = (prefix: string) => (actionType: string) =>
   `${prefix}${actionType}`;
 
+/**
+ * Creates a matchable action creator.
+ *
+ * A matchable action creator has a function called `matches` which can be used in your reducer to
+ * match a specific action.
+ *
+ * @param actionCreator a function that returns an action. Can take any amount of params.
+ *
+ * @example
+ * const increment = createMaction((amount: number) => ({
+ *     type: "INCREMENT",
+ *     amount,
+ * })
+ *
+ * // In reducer
+ * if (increment.matches(action)) {
+ *     // This will be typed as `number`
+ *     const { amount } = action;
+ * }
+ */
 export const createMaction = <ActionCreator extends ActionCreatorFn>(
   actionCreator: ActionCreator
 ): MatchableAction<ActionCreator> =>
@@ -30,6 +63,21 @@ const reduceForActionBuilder =
   ): SecondaryReducer<State, ActionCreator> =>
     reduce;
 
-export const matchedReducerFactory = <State>() => ({
-  reduceForAction: reduceForActionBuilder<State>(),
-});
+/**
+ * Initialises action reducers with state type.
+ *
+ * State type should be provided as a generic argument.
+ *
+ * @returns {<State>(maction: MatchableAction, (state: State, action: MatchableAction) => State) => SecondaryReducer}
+ *
+ * @example
+ * const reduceForAction = matchedReducerFactory<StateType>();
+ *
+ * // The param `increment` must have been created with `createMaction`
+ * const incrementReducer = reduceForAction(increment, (state, action) => ({
+ *     ...state,
+ *     count: state.count + action.value
+ * });
+ */
+export const matchableReducerFactory = <State = void>() =>
+  reduceForActionBuilder<State>();
